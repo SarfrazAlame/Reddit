bc9af80861117a66101e6aec9c7e146fb60f4de0
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-01-18 08:07:21 +0530

    creating post

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,16 +5,11 @@ import { useForm } from "react-hook-form";
 import { PostCreationRequest, Postvalidators } from "@/lib/validators/post";
 import { zodResolver } from "@hookform/resolvers/zod";
 import EditorJS from "@editorjs/editorjs";
-import LinkTool from "@editorjs/link";
-import Header from "@editorjs/header";
-import ImageTool from "@editorjs/image";
-import List from "@editorjs/list";
-import Code from "@editorjs/code";
-import Inlinecode from "@editorjs/inline-code";
-import Table from "@editorjs/table";
-import Embed from "@editorjs/embed";
 
 import { uploadFiles } from "@/lib/uploadthing";
+import { toast } from "./ui/use-toast";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
 interface EditorProps {
   subredditId: string;
@@ -37,11 +32,11 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
   const ref = useRef<EditorJS>();
   const [isMounted, setIsMounted] = useState<boolean>();
 
-  useEffect(()=>{
-    if(typeof window !== 'undefined'){
-      setIsMounted(true)
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsMounted(true);
     }
-  },[])
+  }, []);
 
   const initializeEditor = useCallback(async () => {
     const EditorJS = (await import("@editorjs/editorjs")).default;
@@ -53,77 +48,134 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
     const LinkTool = (await import("@editorjs/link")).default;
     const Inlinecode = (await import("@editorjs/inline-code")).default;
     const ImageTool = (await import("@editorjs/image")).default;
-  }, []);
 
-  if (!ref.current) {
-    const editor = new EditorJS({
-      holder: "editor",
-      onReady() {
-        ref.current = editor;
-      },
-      placeholder: "Type here to write your post",
-      inlineToolbar: true,
-      data: { blocks: [] },
-      tools: {
-        header: Header,
-        linkTool: {
-          class: LinkTool,
-          config: {
-            endpoint: "/api/link",
-          },
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: "editor",
+        onReady() {
+          ref.current = editor;
         },
-        image: {
-          class: ImageTool,
-          config: {
-            uploader: {
-              async uploadByFile(file: File) {
-                const [res] = await uploadFiles([file], "imageUploader");
+        placeholder: "Type here to write your post",
+        inlineToolbar: true,
+        data: { blocks: [] },
+        tools: {
+          header: Header,
+          linkTool: {
+            class: LinkTool,
+            config: {
+              endpoint: "/api/link",
+            },
+          },
+          image: {
+            class: ImageTool,
+            config: {
+              uploader: {
+                async uploadByFile(file: File) {
+                  const [res] = await uploadFiles([file], "imageUploader");
 
-                return {
-                  success: 1,
-                  file: {
-                    url: res.fileUrl,
-                  },
-                };
+                  return {
+                    success: 1,
+                    file: {
+                      url: res.fileUrl,
+                    },
+                  };
+                },
               },
             },
           },
+          list: List,
+          code: Code,
+          inlineCode: Inlinecode,
+          table: Table,
+          embed: Embed,
         },
-        list: List,
-        code: Code,
-        inlineCode: Inlinecode,
-        table: Table,
-        embed: Embed,
-      },
-    });
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsMounted(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (Object.keys[errors].length) {
+      for (const [_key, value] of Object.entries(errors)) {
+        toast({
+          title: "something went wrong",
+          description: (value as { message: string }).message,
+          variant: "destructive",
+        });
+      }
+    }
+  }, [errors]);
+
+  useEffect(() => {
+    const init = async () => {
+      await initializeEditor();
+
+      setTimeout(() => {
+        titleRef.current?.focus();
+      }, 0);
+    };
+
+    if (isMounted) {
+      init();
+
+      return () => {
+        ref.current?.destroy();
+        ref.current = undefined;
+      };
+    }
+  }, [isMounted, initializeEditor]);
+
+  const {} = useMutation({
+    mutationFn: async ({
+      title,
+      content,
+      subredditId,
+    }: PostCreationRequest) => {
+      const paload: PostCreationRequest = {
+        subredditId,
+        title,
+        content,
+      };
+      const {} = await axios.post("/api/subreddit/post/create", paload);
+    },
+  });
+
+  async function onSubmit(data: PostCreationRequest) {
+    const blocks = await ref.current?.save();
+
+    const payload: PostCreationRequest = {
+      title: data.title,
+      content: blocks,
+      subredditId,
+    };
   }
 
-  useEffect(()=>{
-    const init = async () =>{
-      await initializeEditor()
-
-      setTimeout(()=>{
-
-      })
-    }
-
-    if(isMounted){
-      init()
-
-      return () =>{}
-
-    }
-  },[isMounted, initializeEditor])
+  const { ref: titleRef, ...rest } = register("title");
 
   return (
     <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
-      <form id="subreddit-post-form" onSubmit={() => {}} className="w-fit">
+      <form
+        id="subreddit-post-form"
+        onSubmit={() => {
+          handleSubmit((e) => {});
+        }}
+        className="w-fit"
+      >
         <div className="prose prose-stone dark:prose-invert">
           <TextareaAutosize
-            placeholder="Title"
+            ref={(e) => {
+              titleRef(e);
+
+              titleRef.current = e;
+            }}
             className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
           />
-          <div id="editor" className="min-h-[500px]"/>
+          <div id="editor" className="min-h-[500px]" />
         </div>
       </form>
     </div>
